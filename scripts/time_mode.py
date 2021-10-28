8d6477c9cbf92523d64a6fe23471850a3fb45255
sarxell <sarapombinho@ua.pt>
2021-10-28 21:49:26 +0100

    Beginning of the time_mode test

---
+++ scripts/time_mode.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+from colorama import Fore, Back, Style
+import readchar
+from tictoc import *
+import random
+
+
+def time_mode(max_value):
+    print("Program stops after " + Fore.BLUE + str(max_value) + Style.RESET_ALL + ' seconds')
+    print("Click on a letter to start the test")
+
+
+    a = readchar.readkey()
+    tic()
+    t=time()
+
+    # (maybe) first seeing the ascii table and doing a list with all the lower case letters
+    letters = [chr(x) for x in range(97, 123)]
+
+    print(letters)
+    elapsed = time() - t
+
+    while elapsed < max_value:
+        rand_letter = random.choice(letters)
+        print('The program wants' + rand_letter)
+        typed_letter = readchar.readkey()
+        #the toc prints the value and elapsed works for the while cycle
+        elapsed = time() - t
+        toc()
